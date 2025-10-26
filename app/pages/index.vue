d431a4591f2800d3b20af6a7d35af3880863be97
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-10-26 17:15:52 +0800

    隐藏服务器日志入口

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,9 +19,9 @@
 						<button class="btn btn-sm btn-neutral mt-4">
 							<NuxtLink to="/guild" class="flex items-center">公会</NuxtLink>
 						</button>
-						<button class="btn btn-sm btn-neutral mt-4">
+						<!-- <button class="btn btn-sm btn-neutral mt-4">
 							<NuxtLink to="/logs" class="flex items-center">服务器日志</NuxtLink>
-						</button>
+						</button> -->
 					</div>
 				</div>
 			</div>
@@ -58,7 +58,7 @@
 				<NuxtLink to="docs" class="link link-hover">服务器指北</NuxtLink>
 				<NuxtLink to="/litebans" class="link link-hover">小黑屋</NuxtLink>
 				<NuxtLink to="/guild" class="link link-hover">公会</NuxtLink>
-				<NuxtLink to="/logs" class="link link-hover">服务器日志</NuxtLink>
+				<!-- <NuxtLink to="/logs" class="link link-hover">服务器日志</NuxtLink> -->
 			</nav>
 			<nav>
 				<h6 class="footer-title">Social</h6>
